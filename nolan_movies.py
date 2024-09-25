b1d4dfc460f4cb111a3a39606a91cad80d11a01f
Karol Swirniak <karol.swirniak@protonmail.com>
2024-09-25 10:24:31 +0200

    random movi title generator implemented

--- nolan_movies.py
+++ nolan_movies.py
@@ -1,3 +1,7 @@
+from random import choice
+
 movies = ['Tarantella', 'Larceny', 'Doodlebug', 'Following', 'Memento', 'Insomnia', 'Batman Begins', 'Prestige',
           'The Dark Knight', 'Inception', 'The Dark Knight Rises', 'Interstellar', 'Quay', 'Dunkirk', 'Tenet',
           'Oppenheimer']
+
+print(choice(movies))
