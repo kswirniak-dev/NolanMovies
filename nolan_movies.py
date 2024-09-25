3602249715870b770e96ea42b804f47f16c184c5
Karol Swirniak <karol.swirniak@protonmail.com>
2024-09-25 10:17:51 +0200

    list movies created

---
+++ nolan_movies.py
@@ -0,0 +1,3 @@
+movies = ['Tarantella', 'Larceny', 'Doodlebug', 'Following', 'Memento', 'Insomnia', 'Batman Begins', 'Prestige',
+          'The Dark Knight', 'Inception', 'The Dark Knight Rises', 'Interstellar', 'Quay', 'Dunkirk', 'Tenet',
+          'Oppenheimer']
