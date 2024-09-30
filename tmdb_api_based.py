953722739615fe658fc4f56554072d10511cc59d
Karol Swirniak <karol.swirniak@protonmail.com>
2024-09-30 09:28:39 +0200

    implemented TMDB API based random Nolan movie genrator

---
+++ tmdb_api_based.py
@@ -0,0 +1,19 @@
+from random import choice
+import requests
+from nolan_movies import movies
+
+url = "https://api.themoviedb.org/3/search/movie?"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwY2QwNzlmNWYyY2Y1YmZmMDkyZmIxYzBiZmFhOTYxZiIsIm5iZiI6MTcyNzU0MjM1OC4yNTE2NjEsInN1YiI6IjY2ZjgzMWM5MzkzY2RhMWQxZGNjMjM0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KMUJ9o8Y2Nsz-ygVRHWtcT89pXr91MqtVf7u66lyACI"
+}
+
+film = choice(movies)
+querystring = {"query": film["title"], "include_adult": "false", "year": str(film['year'])}
+response = requests.get(url, headers=headers, params=querystring)
+for result in response.json()["results"]:
+    if result["original_title"] == film["title"] and result["release_date"][:4] == str(film["year"]):
+        print(result["original_title"])
+        print(result["overview"])
+
