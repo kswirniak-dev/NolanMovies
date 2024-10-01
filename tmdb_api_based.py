605d8eee5b56f5c39af35fa418b64c3f4257781b
Karol Swirniak <karol.swirniak@protonmail.com>
2024-10-01 13:11:01 +0200

    wrapped finding movie ids into the function

--- tmdb_api_based.py
+++ tmdb_api_based.py
@@ -9,11 +9,18 @@ headers = {
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwY2QwNzlmNWYyY2Y1YmZmMDkyZmIxYzBiZmFhOTYxZiIsIm5iZiI6MTcyNzU0MjM1OC4yNTE2NjEsInN1YiI6IjY2ZjgzMWM5MzkzY2RhMWQxZGNjMjM0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KMUJ9o8Y2Nsz-ygVRHWtcT89pXr91MqtVf7u66lyACI"
 }
 
-film = choice(movies)
-querystring = {"query": film["title"], "include_adult": "false", "year": str(film["year"])}
-response = requests.get(url, headers=headers, params=querystring)
-for result in response.json()["results"]:
-    if result["original_title"] == film["title"] and result["release_date"][:4] == str(film["year"]):
-        print(result["original_title"])
-        print(result["overview"])
 
+def find_all_nolan_movie_ids():
+    movies_ids = []
+    for film in movies:
+        querystring = {"query": film["title"], "include_adult": "false", "year": str(film["year"])}
+        response = requests.get(url, headers=headers, params=querystring)
+        for result in response.json()["results"]:
+            if result["original_title"] == film["title"] and result["release_date"][:4] == str(film["year"]):
+                movies_ids.append(result["id"])
+    return movies_ids
+
+
+nolan_movies_ids = find_all_nolan_movie_ids()
+print(nolan_movies_ids)
+print(len(nolan_movies_ids))
