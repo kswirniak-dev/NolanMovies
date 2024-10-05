7145610b16fae114cf05d20202936f51a53848c8
Karol Swirniak <karol.swirniak@protonmail.com>
2024-10-05 13:37:34 +0200

    defined test methods

--- test_tmbd_api_based.py
+++ test_tmbd_api_based.py
@@ -8,3 +8,9 @@ class TestTmdbApiBased(unittest.TestCase):
 
     def test_find_all_nolan_movie_ids(self):
         pass
+
+    def test_get_search_result_by_title_and_year(self):
+        pass
+
+    def test_get_single_movie_from_search_result(self):
+        pass
