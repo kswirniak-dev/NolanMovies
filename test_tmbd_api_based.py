e2f88f87246b96ae59bb496f14ca093ff68c874c
Karol Swirniak <karol.swirniak@protonmail.com>
2024-10-07 11:03:18 +0200

    implemented setUp method, refactored tests accordingly.

--- test_tmbd_api_based.py
+++ test_tmbd_api_based.py
@@ -4,7 +4,22 @@ import tmdb_api_based
 
 class TestTmdbApiBased(unittest.TestCase):
     def setUp(self):
-        pass
+        self.search_result = {'page': 1,
+                               'results': [{'adult': False,
+                                            'backdrop_path': '/7Wev9JMo6R5XAfz2KDvXb7oPMmy.jpg',
+                                            'genre_ids': [9648, 53],
+                                            'id': 77,
+                                            'original_language': 'en',
+                                            'original_title': 'Memento',
+                                            'overview': "Leonard Shelby is tracking down the man who raped and murdered his wife. The difficulty of locating his wife's killer, however, is compounded by the fact that he suffers from a rare, untreatable form of short-term memory loss. Although he can recall details of life before his accident, Leonard cannot remember what happened fifteen minutes ago, where he's going, or why.",
+                                            'popularity': 31.486, 'poster_path': '/nWtySDlffTfwAa0rSfq61o33ZXV.jpg',
+                                            'release_date': '2000-10-11', 'title': 'Memento', 'video': False,
+                                            'vote_average': 8.181, 'vote_count': 14716}],
+                               'total_pages': 1,
+                               'total_results': 1}
+        self.empty_search_result = {}
+        self.film_memento = {"title": "Memento", "year": 2000}
+        self.film_insomnia = {"title": "Insomnia", "year": 2002}
 
     def test_find_all_nolan_movie_ids(self):
         pass
@@ -13,52 +28,22 @@ class TestTmdbApiBased(unittest.TestCase):
         pass
 
     def test_get_single_movie_from_search_result_success(self):
-        setup_search_result = {'page': 1,
-                               'results': [{'adult': False,
-                                            'backdrop_path': '/7Wev9JMo6R5XAfz2KDvXb7oPMmy.jpg',
-                                            'genre_ids': [9648, 53],
-                                            'id': 77,
-                                            'original_language': 'en',
-                                            'original_title': 'Memento',
-                                            'overview': "Leonard Shelby is tracking down the man who raped and murdered his wife. The difficulty of locating his wife's killer, however, is compounded by the fact that he suffers from a rare, untreatable form of short-term memory loss. Although he can recall details of life before his accident, Leonard cannot remember what happened fifteen minutes ago, where he's going, or why.",
-                                            'popularity': 31.486, 'poster_path': '/nWtySDlffTfwAa0rSfq61o33ZXV.jpg',
-                                            'release_date': '2000-10-11', 'title': 'Memento', 'video': False,
-                                            'vote_average': 8.181, 'vote_count': 14716}],
-                               'total_pages': 1,
-                               'total_results': 1}
-        setup_film = {"title": "Memento", "year": 2000}
         expected = 77
-        result = tmdb_api_based.get_single_movie_from_search_result(setup_search_result, setup_film)
+        result = tmdb_api_based.get_single_movie_from_search_result(self.search_result, self.film_memento)
 
         self.assertIsNotNone(result)
         self.assertEqual(result, expected)
 
     def test_get_single_movie_from_search_result_fail(self):
-        setup_search_result = {'page': 1,
-                               'results': [{'adult': False,
-                                            'backdrop_path': '/7Wev9JMo6R5XAfz2KDvXb7oPMmy.jpg',
-                                            'genre_ids': [9648, 53],
-                                            'id': 77,
-                                            'original_language': 'en',
-                                            'original_title': 'Memento',
-                                            'overview': "Leonard Shelby is tracking down the man who raped and murdered his wife. The difficulty of locating his wife's killer, however, is compounded by the fact that he suffers from a rare, untreatable form of short-term memory loss. Although he can recall details of life before his accident, Leonard cannot remember what happened fifteen minutes ago, where he's going, or why.",
-                                            'popularity': 31.486, 'poster_path': '/nWtySDlffTfwAa0rSfq61o33ZXV.jpg',
-                                            'release_date': '2000-10-11', 'title': 'Memento', 'video': False,
-                                            'vote_average': 8.181, 'vote_count': 14716}],
-                               'total_pages': 1,
-                               'total_results': 1}
-        setup_film = {"title": "Insomnia", "year": 2002}
         expected = None
-        result = tmdb_api_based.get_single_movie_from_search_result(setup_search_result, setup_film)
+        result = tmdb_api_based.get_single_movie_from_search_result(self.search_result, self.film_insomnia)
 
         self.assertIsNone(result)
         self.assertEqual(result, expected)
 
     def test_get_single_movie_from_search_result_value_error(self):
-        setup_search_result = {}
-        setup_film = {"title": "Insomnia", "year": 2002}
         with self.assertRaises(ValueError):
-            tmdb_api_based.get_single_movie_from_search_result(setup_search_result, setup_film)
+            tmdb_api_based.get_single_movie_from_search_result(self.empty_search_result, self.film_insomnia)
 
 
 
